29f327af8eb5cb71f7f6a21392c8a472ef781faa
monsta <yell.bondarenko@gmail.com>
2018-12-05 11:13:51 +0100

    store model dirpaths in variable

--- utils/tf_model/tf_model_test.py
+++ utils/tf_model/tf_model_test.py
@@ -28,8 +28,12 @@ class TestModelWithSummary(TestModel):
 
 
 class TestTensorFlowModel(tf.test.TestCase):
+    def __init__(self, *args, **kwargs):
+        self.model_dirpaths = ['test_model_{}'.format(i) for i in xrange(2)]
+        super(TestTensorFlowModel, self).__init__(*args, **kwargs)
+
     def cleanup(self):
-        for d in ('test_model_1/', 'test_model_2/'):
+        for d in self.model_dirpaths:
             try:
                 rmtree(d)
             except OSError:
@@ -71,7 +75,7 @@ class TestTensorFlowModel(tf.test.TestCase):
         assert not os.path.isdir(m.val_summary_dirpath)
 
     def test_summaries_created(self):
-        m = TestModelWithSummary(model_dirpath='test_model_1')
+        m = TestModelWithSummary(model_dirpath=self.model_dirpaths[0])
 
         assert m.tf.merged_summaries is not None
         assert m.tf.train_writer is not None
@@ -106,16 +110,16 @@ class TestTensorFlowModel(tf.test.TestCase):
             assert m.tf.session is sess
 
     def test_save_load(self):
-        m = TestModel(model_dirpath='test_model_1')
+        m = TestModel(model_dirpath=self.model_dirpaths[0])
         m.global_step = 42
         m.save_tf_model()
 
         tf.reset_default_graph()
-        m2 = TestModel(model_dirpath='test_model_1', load_latest=True)
+        m2 = TestModel(model_dirpath=self.model_dirpaths[0], load_latest=True)
         assert m2.global_step == 42, (m2.global_step)
 
     def test_save_load_specific_checkpoint(self):
-        m = TestModel(model_dirpath='test_model_1')
+        m = TestModel(model_dirpath=self.model_dirpaths[0])
         for i in xrange(5):
             m.run(m.op)
             m.global_step += 10
@@ -124,39 +128,39 @@ class TestTensorFlowModel(tf.test.TestCase):
         for i in xrange(5):
             tf.reset_default_graph()
             global_step = 10 * (i + 1)
-            m = TestModel(model_dirpath='test_model_1', global_step=global_step, load_latest=True)
+            m = TestModel(model_dirpath=self.model_dirpaths[0], global_step=global_step, load_latest=True)
             assert m.run(m.x) == i + 1
             assert m.global_step == global_step
 
     def test_multiple_models_separate_graphs(self):
-        m1 = TestModel(model_dirpath='test_model_1')
+        m1 = TestModel(model_dirpath=self.model_dirpaths[0])
         m1.run(m1.x.assign(42))
         m1.save_tf_model()
 
         tf.reset_default_graph()
 
-        m2 = TestModel(model_dirpath='test_model_2')
+        m2 = TestModel(model_dirpath=self.model_dirpaths[1])
         m2.run(m2.x.assign(1337))
         m2.save_tf_model()
 
         tf.reset_default_graph()
 
         with tf.Graph().as_default():
-            m1 = TestModel(model_dirpath='test_model_1', load_latest=True)
+            m1 = TestModel(model_dirpath=self.model_dirpaths[0], load_latest=True)
             assert m1.run(m1.x) == 42
 
         with tf.Graph().as_default():
-            m2 = TestModel(model_dirpath='test_model_2', load_latest=True)
+            m2 = TestModel(model_dirpath=self.model_dirpaths[1], load_latest=True)
             assert m2.run(m2.x) == 1337
 
     def test_multiple_models_separate_graphs_and_sessions(self):
-        m1 = TestModel(model_dirpath='test_model_1')
+        m1 = TestModel(model_dirpath=self.model_dirpaths[0])
         m1.run(m1.x.assign(42))
         m1.save_tf_model()
 
         tf.reset_default_graph()
 
-        m2 = TestModel(model_dirpath='test_model_2')
+        m2 = TestModel(model_dirpath=self.model_dirpaths[1])
         m2.run(m2.x.assign(1337))
         m2.save_tf_model()
 
@@ -164,37 +168,37 @@ class TestTensorFlowModel(tf.test.TestCase):
 
         with tf.Graph().as_default():
             with tf.Session().as_default():
-                m1 = TestModel(model_dirpath='test_model_1', load_latest=True)
+                m1 = TestModel(model_dirpath=self.model_dirpaths[0], load_latest=True)
                 assert m1.run(m1.x) == 42
 
         with tf.Graph().as_default():
             with tf.Session().as_default():
-                m2 = TestModel(model_dirpath='test_model_2', load_latest=True)
+                m2 = TestModel(model_dirpath=self.model_dirpaths[1], load_latest=True)
                 assert m2.run(m2.x) == 1337
 
     def test_multiple_models_variable_scopes(self):
         with tf.variable_scope('model1'):
-            m1 = TestModel(model_dirpath='test_model_1')
+            m1 = TestModel(model_dirpath=self.model_dirpaths[0])
             m1.run(m1.x.assign(42))
             m1.save_tf_model()
 
         with tf.variable_scope('model2'):
-            m2 = TestModel(model_dirpath='test_model_2')
+            m2 = TestModel(model_dirpath=self.model_dirpaths[1])
             m2.run(m2.x.assign(1337))
             m2.save_tf_model()
 
         tf.reset_default_graph()
 
         with tf.variable_scope('model1'):
-            m1 = TestModel(model_dirpath='test_model_1', load_latest=True)
+            m1 = TestModel(model_dirpath=self.model_dirpaths[0], load_latest=True)
             assert m1.run(m1.x) == 42
 
         with tf.variable_scope('model2'):
-            m2 = TestModel(model_dirpath='test_model_2', load_latest=True)
+            m2 = TestModel(model_dirpath=self.model_dirpaths[1], load_latest=True)
             assert m2.run(m2.x) == 1337
 
     def test_partial_save_load_via_list(self):
-        m = TestModel(model_dirpath='test_model_1')
+        m = TestModel(model_dirpath=self.model_dirpaths[0])
         m.tf_latest_saver = tf.train.Saver(var_list=[m.x, m.y])  # save only x, y
         m.global_step = 1111
         m.run(m.x.assign(2222))
@@ -204,7 +208,7 @@ class TestTensorFlowModel(tf.test.TestCase):
 
         tf.reset_default_graph()
 
-        m = TestModel(model_dirpath='test_model_1')
+        m = TestModel(model_dirpath=self.model_dirpaths[0])
         # here if corresponding `var_list` is not provided,
         # an error will be raised since saver will expect
         # to find all the variables in the checkpoint, including
@@ -220,7 +224,7 @@ class TestTensorFlowModel(tf.test.TestCase):
         assert_almost_equal(0., m.run(m.z))
 
     def test_partial_save_load_via_dict(self):
-        m = TestModel(model_dirpath='test_model_1')
+        m = TestModel(model_dirpath=self.model_dirpaths[0])
         m.tf_latest_saver = tf.train.Saver(var_list={'x': m.x, 'y': m.y})  # save only x, y
         m.global_step = 1111
         m.run(m.x.assign(2222))
@@ -230,7 +234,7 @@ class TestTensorFlowModel(tf.test.TestCase):
 
         tf.reset_default_graph()
 
-        m = TestModel(model_dirpath='test_model_1')
+        m = TestModel(model_dirpath=self.model_dirpaths[0])
         m.tf_latest_saver = tf.train.Saver(var_list={'x': m.x, 'y': m.y})
         TensorFlowModel.load_tf_model(m.model_dirpath,
                                       m.tf.session,
@@ -242,7 +246,7 @@ class TestTensorFlowModel(tf.test.TestCase):
         assert_almost_equal(0., m.run(m.z))
 
     def test_run_summary(self):
-        m = TestModelWithSummary(model_dirpath='test_model_1')
+        m = TestModelWithSummary(model_dirpath=self.model_dirpaths[0])
 
         m.run_summary(train=True)
         assert os.listdir(m.train_summary_dirpath)
