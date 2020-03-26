6a87fdab9f8639f9aa1d2e1e6a18952ad4dcef64
Yelysei Bondarenko <yell.bondarenko@gmail.com>
2020-03-26 16:26:02 +0100

    improve Stopwatch design + upgrade to python3

--- utils/stopwatch.py
+++ utils/stopwatch.py
@@ -4,75 +4,72 @@ import time
 
 class Stopwatch(object):
     """
-    A simple cross-platform
-    context-manager stopwatch.
+    A simple cross-platform context-manager stopwatch.
 
     Examples
     --------
     >>> import time
     >>> with Stopwatch(verbose=True) as s:
-    ...     time.sleep(0.1) # doctest: +ELLIPSIS
+    ...     time.sleep(0.101) # doctest: +ELLIPSIS
     Elapsed time: 0.10... sec
-    >>> with Stopwatch(verbose=False) as s:
-    ...     time.sleep(0.1)
-    >>> import math
-    >>> math.fabs(s.elapsed() - 0.1) < 0.05
-    True
     """
     def __init__(self, verbose=False):
-        self.verbose = verbose
+        self._verbose = verbose
+
+        self._start_time_point = 0.
+        self._total_duration = 0.
+        self._is_running = False
+
         if sys.platform == 'win32':
             # on Windows, the best timer is time.clock()
-            self._timer_func = time.clock
+            self._timer_fn = time.clock
         else:
             # on most other platforms, the best timer is time.time()
-            self._timer_func = time.time
-        self.reset()
-
+            self._timer_fn = time.time
+    
     def __enter__(self, verbose=False):
         return self.start()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
-        return self.elapsed()
+        if self._verbose:
+            self._print()
+        return self._total_duration
 
     def start(self):
         if not self._is_running:
-            self._start = self._timer_func()
+            self._start_time_point = self._timer_fn()
             self._is_running = True
         return self
 
     def stop(self):
         if self._is_running:
-            self._total += (self._timer_func() - self._start)
+            self._total_duration += (self._timer_fn() - self._start_time_point)
             self._is_running = False
         return self
 
-    def elapsed(self):
-        if self._is_running:
-            now = self._timer_func()
-            self._total += (now - self._start)
-            self._start = now
-        if self.verbose:
-            print "Elapsed time: {0:.3f} sec".format(self._total)
-        return self._total
-
     def reset(self):
-        self._start = 0.
-        self._total = 0.
+        self._start_time_point = 0.
+        self._total_duration = 0.
         self._is_running = False
         return self
 
+    def _update_state(self):
+        now = self._timer_fn()
+        self._total_duration += (now - self._start_time_point)
+        self._start_time_point = now
 
-if __name__ == '__main__':
-    s = Stopwatch()
-    s.start()
-    time.sleep(0.1)
-    print s.elapsed()
-    time.sleep(0.1)
-    print s.elapsed()
-    time.sleep(0.1)
-    print s.elapsed()
-    s.stop()
-    print s.elapsed()
-    print s.reset().elapsed()
+    def _print(self):
+        s = 'Elapsed time: {0:.3f} sec'.format(self._total_duration)
+        print(s)
+
+    def print(self):
+        if self._is_running:
+            self._update_state()
+        self._print()
+
+    def get_total_duration(self):
+        if self._is_running:
+            self._update_state()
+        return self._total_duration
+   
